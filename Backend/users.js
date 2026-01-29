badae905983562f89056b5908203cb8896b5003b
anild <dasarwaranilc@gmail.com>
2026-01-29 19:05:37 +0530

    GET AND DELETE METHOD

--- Backend/users.js
+++ Backend/users.js
@@ -29,4 +29,44 @@ router.get('/users/:user_name', async (req, res) => {
     }
 });
 
+// Create a new user
+router.post('/users', async (req, res) => {
+    const { user_name, pwd, email_id, added_on, added_by, updated_on, updated_by, is_active, user_role } = req.body;
+
+    try {
+        const result = await context.post(
+            'INSERT INTO [dbo].[tbl_users] ([user_name] ,[pwd],[email_id],[added_on],[added_by],[updated_on],[updated_by],[is_active],[user_role])VALUES (@user_name, @pwd, @email_id, @added_on, @added_by, @updated_on, @updated_by, @is_active, @user_role)',
+            [
+                { name: 'user_name', type: sql.VarChar, value: user_name },
+                { name: 'pwd', type: sql.VarChar, value: pwd },
+                { name: 'email_id', type: sql.VarChar, value: email_id },
+                { name: 'added_on', type: sql.DateTime2, value: added_on },
+                { name: 'added_by', type: sql.VarChar, value: added_by },
+                { name: 'updated_on', type: sql.DateTime2, value: updated_on },
+                { name: 'updated_by', type: sql.VarChar, value: updated_by },
+                { name: 'is_active', type: sql.VarChar, value: is_active },
+                { name: 'user_role', type: sql.VarChar, value: user_role }
+            ]
+        );
+        res.json(result);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+});
+
+router.delete('/users/:user_name', async (req, res) => {
+    const { user_name } = req.params;
+    try {
+        const result = await context.delete(
+            'DELETE FROM [dbo].[tbl_users] WHERE user_name = @user_name',
+            [
+                { name: 'user_name', type: sql.VarChar, value: user_name }
+            ]
+        );
+        res.json(result);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
