ca27eb1b54af93e6ee71b9d7a8a00082e7f49cad
BhangeKshitija <154862175+BhangeKshitija@users.noreply.github.com>
2026-02-01 14:15:13 +0530

    Create users.js

---
+++ Backend/users.js
@@ -0,0 +1,23 @@
+const sql = require('mssql/msnodesqlv8');
+
+const config = {
+  server: "DESKTOP-INSTJ3I",
+  database: "class_data",
+  options: {
+    trustedConnection: true, // Set to true if using Windows Authentication
+    trustServerCertificate: true, // Set to true if using self-signed certificates
+  },
+  // driver: "ODBC Driver 18 for SQL Server", // Uncomment to use specific driver
+};
+
+(async () => {
+  try {
+    await sql.connect(config);
+    const result = await sql.query`select TOP 10 * from dbo.tbl_users`;
+    console.dir(result.recordset);
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
+module.exports = {sql};
